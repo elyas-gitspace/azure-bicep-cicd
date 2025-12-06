0cd34541c697ca04071addcadf7113da478c8b42
elyas <meziani.elyastech@gmail.com>
2025-12-06 16:09:30 +0000

    Update: modif du deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #commandes que j'ai trouvée sur la doc officielle Azure : https://learn.microsoft.com/en-us/azure/azure-resource-manager/bicep/deploy-cli 
+#!/bin/bash
+
 az group create --name webapp-project-rg --location "westeurope"
 
 az deployment group create \
@@ -9,11 +11,11 @@ az deployment group create \
     --template-file $GITHUB_WORKSPACE/infrastructure/main.bicep \
     --parameters elyassWebApp=elyassWebApp \
                  elyassAppServicePlan=elyassAppServicePlan \
-                 elyassStorageAccount=elyassStorageAccount \
-                 storageAccountType=Standard_LRS
-
+                 elyassStorageAccount=elyassstorageacc \
+                 storageAccountsku=Standard_LRS
 
 echo "Déploiement terminé avec succès !"
+
 
 
 
