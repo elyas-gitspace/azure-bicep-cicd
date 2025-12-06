b9ae011f8f6c718ea7ef097f7c13bbbbe74baa97
elyas <meziani.elyastech@gmail.com>
2025-12-06 15:24:14 +0000

    Update: Définition des paramètres du bicep dans mon deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,14 @@
 az group create --name webapp-project-rg --location "westeurope"
 
 az deployment group create \
-	--name bicep-deployment \
-	--resource-group webapp-project-rg \
-	--template-file ../infrastructure/main.bicep \
-	--parameters storageAccountType=Standard_LRS
+    --name bicep-deployment \
+    --resource-group webapp-project-rg \
+    --template-file ../infrastructure/main.bicep \
+    --parameters elyassWebApp=elyassWebApp \
+                 elyassAppServicePlan=elyassAppServicePlan \
+                 elyassStorageAccount=elyassStorageAccount \
+                 storageAccountType=Standard_LRS
+
 
 echo "Déploiement terminé avec succès !"
 
